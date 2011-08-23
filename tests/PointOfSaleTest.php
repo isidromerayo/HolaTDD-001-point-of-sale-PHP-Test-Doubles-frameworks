61ac033709d82fe553a3ca526aef3b0f85aaed9e
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-23 18:15:46 +0200

    Define first test and Mockito assertion to implement in PHP / Definir el primer test y la aserción con Mockito a implementar en PHP

---
+++ tests/PointOfSaleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * @author Isidro Merayo
+ */
+class PointOfSaleTest extends PHPUnit_Framework_TestCase {
+ 
+    /**
+     * @test
+     */
+    public function onBarcode_search_catalog()
+    {
+        $this->markTestSkipped();
+        // Java Mockito
+        // verify(catalog).search("123");
+    }
+}
