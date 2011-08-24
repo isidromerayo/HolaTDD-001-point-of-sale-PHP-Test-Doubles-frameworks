57d01ca9cdbdb476722dbbcb715bd1e4638238e9
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2011-08-24 18:15:36 +0200

    Second test on GREEN/Segundo test en VERDE

--- tests/PointOfSaleTest.php
+++ tests/PointOfSaleTest.php
@@ -12,10 +12,34 @@ class PointOfSaleTest extends PHPUnit_Framework_TestCase {
     public function onBarcode_search_catalog()
     {
         $catalog = $this->getMock('Catalog');
-        $pointOfSale = new PointOfSale($catalog);
+        $screen = $this->getMock('Screen');
+        
+        $pointOfSale = new PointOfSale($catalog, $screen);
         
         // verify(catalog).search("123");
         $catalog->expects($this->once())->method('search')->with('123');
+                
+        $pointOfSale->onBarcode('123');
+    }
+    /**
+     * @test
+     */
+    public function onBarcode_show_price() {
+        $catalog = $this->getMock('Catalog');
+        $screen = $this->getMock('Screen');
+        
+        // when(catalog.search("123").thenReturn("1€")
+        $catalog->expects($this->once())
+                ->method('search')
+                ->with('123')
+                ->will($this->returnValue('1€'));
+                
+        $pointOfSale = new PointOfSale($catalog, $screen);
+        
+        // verify(screen).show("1€");
+        $screen->expects($this->once())
+                ->method('show')
+                ->with('1€');
                 
         $pointOfSale->onBarcode('123');
     }
