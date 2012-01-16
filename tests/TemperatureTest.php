39aa855cfd86943b968ba92fc48bc447d2ddbff9
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-01-16 20:23:29 +0100

    add example from Mockery's documentation/añadido ejemplo desde la documentación de Mockery

---
+++ tests/TemperatureTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @author Isidro Merayo
+ */
+use \Mockery as m;
+
+class TemperatureTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @test 
+     */
+    public function getsAverageTemperatureFromThreeServiceReadings() {
+        $service = m::mock('service');
+        $service->shouldReceive('readTemp')->times(3)->andReturn(10, 12, 14);
+        $temperature = new Temperature($service);
+        $this->assertEquals(12, $temperature->average());
+    }
+
+}
