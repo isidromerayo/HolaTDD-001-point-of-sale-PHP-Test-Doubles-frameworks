ef0d4038783a428b2b4dda2ac8febf06e45b1358
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-01-21 18:58:44 +0100

    example Phake in RED/ejemplo de Phake en ROJO

--- tests/MyMockedClassTest.php
+++ tests/MyMockedClassTest.php
@@ -6,14 +6,11 @@ class MyMockedClassTest extends PHPUnit_Framework_TestCase {
     {
         $mock = $this->getMock('MyMockedClass');
 
-        $mock->expects($this->at(0))->method('fooWithArgument')
-            ->with('foo');
+        $mock->expects($this->at(0))->method('fooWithArgument')->with('foo');
 
-        $mock->expects($this->at(1))->method('fooWithArgument')
-            ->with('bar');
+        $mock->expects($this->at(1))->method('fooWithArgument')->with('bar');
 
-        $mock->fooWithArgument('bar');
         $mock->fooWithArgument('foo');
-        
+        $mock->fooWithArgument('bar');
     }
 }
