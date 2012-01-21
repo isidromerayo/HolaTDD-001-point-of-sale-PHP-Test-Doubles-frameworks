1d70d7fecaa1b5ebac88df25025882407da3247e
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-01-21 18:32:10 +0100

    GREEN test/VERDE

--- tests/MyPhakeMockedClassTest.php
+++ tests/MyPhakeMockedClassTest.php
@@ -5,8 +5,8 @@ class MyPhakeMockedClassTest extends PHPUnit_Framework_TestCase {
     public function testPHPUnitMock() {
         $mock = Phake::mock('MyPhakeMockedClass');
 
-        $mock->fooWithArgument('bar');
         $mock->fooWithArgument('foo');
+        $mock->fooWithArgument('bar');
 
         Phake::inOrder(
         Phake::verify($mock)->fooWithArgument('foo'),
